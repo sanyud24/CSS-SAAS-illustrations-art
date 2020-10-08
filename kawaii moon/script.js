9f50ed54ef1bdf37aecf257eb5a0c1ab194a81cc
sanyu daver <57134307+sanyud24@users.noreply.github.com>
2020-10-08 11:30:21 +0530

    Update script.js

--- kawaii moon/script.js
+++ kawaii moon/script.js
@@ -1,9 +1,1 @@
-var moon= document.getElementById("moon");
-function shake(){
-    if( moon.classList !="animate"){
-    moon.classList.add("animate");
-    }setTimeout(function(){
-        moon.classList.remove("animate");
-            },500);
-           }
-        
+
